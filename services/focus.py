4c325608be550ff2c443b6ed7d63db5e23a0d8a3
AlissonSteffens <ali.steffens@gmail.com>
2020-06-02 16:44:03 -0300

    :bug: fix direction

--- services/focus.py
+++ services/focus.py
@@ -10,50 +10,82 @@ class FocusFinder:
     def find_direction(self, marks, as_np = False):
         tb = []
         for face in marks:
-            dist_esq = abs(face[1][0] - face[5][0]) #x nariz menos x esq boca
-            dist_dir = abs(face[1][0] - face[4][0]) #x nariz - x direita boca
+            dist_boca_esq = abs(face[1][0] - face[5][0]) #x nariz menos x esq boca
+            dist_boca_dir = abs(face[1][0] - face[4][0]) #x nariz - x direita boca
             
-            dist_o_esq = abs(face[1][1] - face[3][1]) #y nariz - y esquerda olho
-            dist_o_dir = abs(face[1][1] - face[2][1]) #y nariz - y direita olho
+            dist_y_o_esq = abs(face[1][1] - face[3][1]) #y nariz - y esquerda olho
+            dist_y_o_dir = abs(face[1][1] - face[2][1]) #y nariz - y direita olho
+
+            dist_x_o_esq = abs(face[1][0] - face[3][0]) #x nariz - x esquerda olho
+            dist_x_o_dir = abs(face[1][0] - face[2][0]) #x nariz - x direita olho
+
             dist_queixo = abs(face[1][1] - face[0][1]) #y nariz - y queixo
 
 
-            dist_olhos = ((dist_o_esq+dist_o_dir)/2)
+            dist_esq = (dist_x_o_esq + dist_boca_esq)/2
+            dist_dir = (dist_x_o_dir + dist_boca_dir)/2
+
+            dist_olhos = ((dist_y_o_esq+dist_y_o_dir)/2)
             percentual_lateral = dist_esq/(dist_esq+dist_dir)
 
-            percentual_vertical = ((dist_o_esq+dist_o_dir)/2)/dist_queixo
-            direita = False if dist_esq > dist_dir else True
-            frente = True if  (percentual_lateral < .65 and percentual_lateral > .35) else False
+            percentual_vertical = ((dist_y_o_esq+dist_y_o_dir)/2)/dist_queixo
+            
 
-            cima = True if percentual_vertical < .75 else False
-            reto = True if  (percentual_vertical < .9 and percentual_vertical > .5) else False
+            up = fuzz.membership.gaussmf(percentual_vertical,0,.25)
+            middle = fuzz.membership.gaussmf(percentual_vertical,.8, .15)
+            down = fuzz.membership.gaussmf(percentual_vertical,1,.25)
+
+            
+
+            right = fuzz.membership.gaussmf(percentual_lateral,0,.25)
+            center = fuzz.membership.gaussmf(percentual_lateral,.5,.06)
+            left = fuzz.membership.gaussmf(percentual_lateral,1,.25)
+            
+            if right > center and right > left:
+                direction_h = 'Esquerda'
+            else:
+                if left > center and left > right:
+                    direction_h = 'Direita'
+                else:
+                    direction_h = 'Frente'
+            
+            if up > middle and up > down:
+                direction_v = 'Cima'
+            else:
+                if down > middle and down > up:
+                    direction_v = 'Baixo'
+                else:
+                    direction_v = 'Meio' 
+
             tb.append({
                 'distances':{
-                    'distance_nose_to_left_mouth':int(dist_esq),
-                    'distance_nose_to_right_mouth':int(dist_dir),
-                    'distance_nose_to_left_eye': int(dist_o_esq),
-                    'distance_nose_to_rught_eye': int(dist_o_dir),
+                    'h_distance_nose_to_left_mouth':int(dist_boca_esq),
+                    'h_distance_nose_to_right_mouth':int(dist_boca_dir),
+                    'h_distance_nose_to_left_eye': int(dist_x_o_esq),
+                    'h_distance_nose_to_right_eye': int(dist_x_o_dir),
+                    'v_distance_nose_to_left_eye': int(dist_y_o_esq),
+                    'v_distance_nose_to_right_eye': int(dist_y_o_dir),
                     'distance_nose_to_eyes': int(dist_olhos),
                     'distance_nose_to_chin': int(dist_queixo),
                 },
                 'calculated' :{
-                    'x_rate': float(percentual_vertical),
-                    'y_rate': float(percentual_lateral),
+                    'x_rate': float(percentual_lateral),
+                    'y_rate': float(percentual_vertical),
                 },          
                 'fuzzy_is_looking':{
                     'vertical':{
-                        'up': fuzz.membership.gaussmf(percentual_vertical,0,.25),
-                        'middle': fuzz.membership.gaussmf(percentual_vertical,.8, .15),
-                        'down': fuzz.membership.gaussmf(percentual_vertical,1,.25)
+                        'up': up,
+                        'middle': middle,
+                        'down': down
                     },
                     'horizontal':{  
-                        'right': fuzz.membership.gaussmf(percentual_lateral,0,.25),
-                        'center': fuzz.membership.gaussmf(percentual_lateral,.5,.1),
-                        'left': fuzz.membership.gaussmf(percentual_lateral,1,.25)
+                        'right': right,
+                        'center': center,
+                        'left': left
                     }
                 },
-                'direction_h':'Frente' if frente else ('Esquerda' if direita else 'Direita'),
-                'direction_v':'Reto' if reto else ('Cima' if cima else 'Baixo')
+                'direction_h': direction_h,
+                'direction_v': direction_v
             })
 
         json_dump = json.dumps(tb)
